a9067c0867c501320bc827046b7ad728e093e30a
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-17 14:00:10 +0100

    adding all correction

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
 	require_once("connexion.config.php");
+	
+	try {
+		$dbc = new PDO("mysql:host=".$dbhost.";dbname=".$dbname, $dbuser, $dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+	}
+	catch (PDOException $e) {
+		print "Erreur !: " . $e->getMessage() . "<br/>";
+		die();
+	}
+	
+	$query = $dbc->query("SELECT * FROM departements");
+	$resultats = $query->fetchAll();
+	
+	foreach ($resultats as $res) {
+		echo($res["nom_departement"]."<br>");
+	}
+	
+	for ($i=0 ; $i<count($resultats) ; $i++) {
+		echo($resultats[$i]."<br>");
+	}
+	
+	while ($resultat = $query->fetch()) {
+		echo $resultat["nom_departement"];
+	}
